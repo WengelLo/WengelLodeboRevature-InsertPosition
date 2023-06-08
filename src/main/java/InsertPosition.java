11c7a82f1fdabfdcb66d3e81ec64b021ea692017
Wengel Lodebo <tes.wengel@gmail.com>
2023-06-08 03:16:05 +0000

    idonunde

--- src/main/java/InsertPosition.java
+++ src/main/java/InsertPosition.java
@@ -15,6 +15,17 @@ public class InsertPosition {
      * @return return the index that a number should be inserted into a sorted array.
      */
     public int searchInsert(int[] nums, int target) {
-        return -1;
+        int l = 0, h = nums.length - 1;
+        while (l <= h) {
+            int mid = l + (h - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+            } else {
+                h = mid - 1;
+            }
+        }
+        return l;
     }
 }
